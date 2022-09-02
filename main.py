627c42e592c08dc6b2d9f3dd425deb661e13cee7
Vincent <100126727+creativeson@users.noreply.github.com>
2022-09-03 00:52:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 class model_input(BaseModel):
     # marriage_couples: float
     # female_marriage_age: float
