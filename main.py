7010a1aac778a82a56465dff75d1a5f8a9b27aa9
Vincent <100126727+creativeson@users.noreply.github.com>
2022-09-02 22:42:15 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import pickle
+import json
+
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class model_input(BaseModel):
+    # marriage_couples: float
+    # female_marriage_age: float
+    # male_marriage_age: float
+    marriage_couples: int
+    female_marriage_age: int
+    male_marriage_age: int
+
+
+# loading the saved model
+predict_model = pickle.load(open('predict_model.sav', 'rb'))
+
+@app.post('/prediction')
+def babies_pred(input_parameters: model_input):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+
+    marriage = input_dictionary['marriage_couples']
+    f_age = input_dictionary['female_marriage_age']
+    m_age = input_dictionary['male_marriage_age']
+
+    input_list = [marriage, f_age, m_age]
+
+    prediction = predict_model.predict([input_list])
+
+    if prediction[0][0] > 0:
+        return prediction[0][0]
+    else:
+        return "Zero taiwanese baby was born"
+
+
